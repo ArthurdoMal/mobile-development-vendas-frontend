ac6c7997294addce0a77bc204c0e2d48e23adc8f
Arthur Monteiro <114961204+ArthurdoMal@users.noreply.github.com>
2024-11-25 19:43:37 -0300

    grafico

---
+++ grafico123/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./index.css"; 
+import VendasGraficos from "./components/VendasGraficos";
+
+const App = () => {
+  return (
+    <div className="App">
+      <VendasGraficos />
+    </div>
+  );
+};
+
+export default App;
